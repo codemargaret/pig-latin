593f336773faeeee45986667b27eb65428a7b955
Margaret Berry <codeberry1@gmail.com>
2017-12-03 10:58:27 -0800

    remove alerts and commented code

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,21 @@ function translator(userSentence) {
   var words = userSentence.toLowerCase().split(" ");
 
   var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-  // alert(consonants);
   var vowels = ["a", "e", "i", "o", "u"];
 
   for (var i = 0; words.length > i; i +=1) {
     var firstLetter = words[i].charAt(0);
     var secondLetter = words[i].charAt(1);
     var thirdLetter = words[i].charAt(2);
-    alert(firstLetter);
 
     if (firstLetter === 'a' || firstLetter === 'e' || firstLetter === 'i' || firstLetter === 'o' || firstLetter === 'u') {
       words[i] = words[i] + "way"
-      alert(words[i]);
     } else if (firstLetter !== 'a' || firstLetter !== 'e' || firstLetter !== 'i' || firstLetter !== 'o' || firstLetter !== 'u') {
       words[i] = words[i].slice(1) + firstLetter + "ay";
     }
   }
   return words.join(" ");
 };
-
 
 // User Interface
 $(document).ready(function() {
@@ -34,29 +30,3 @@ $(document).ready(function() {
     $("#result").show();
   });
 });
-
-
-
-
-
-// function translator(sentence) {
-
-//   // var word = words[0];
-// //   // var firstLetter = words.charAt(0);
-// //
-// // // for (var i = 0; words.length > i; i +=1) {
-// // //   if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
-// // //     word[i] + "way";
-// // //   }
-//   // }
-// }
-
-  // alert(letter);
-// var letter = userSentence[0];
-
-
-// for (ii = 0; words.length > ii; ii +=1) {
-//   if (firstLetter === consonants) {
-//     words[i] = words[i].slice(vowels) + words[i] + "ay";
-//   }
-// }
